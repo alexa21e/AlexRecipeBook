b3cf870960612d3a18d6cf11d7ff9d07d1e0ca82
alexa21e <pirvuletu.e.alexandra@gmail.com>
2024-11-11 09:36:21 +0200

    Initial setup for API project

---
+++ AlexRecipeBookAPI/AlexRecipeBookAPI/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+	app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
