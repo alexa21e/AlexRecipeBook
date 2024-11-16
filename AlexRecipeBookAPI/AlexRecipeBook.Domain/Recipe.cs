cb08d799305bfa4d3735aca35c8e81275b03aa7f
alexa21e <pirvuletu.e.alexandra@gmail.com>
2024-11-16 14:03:25 +0200

    Add domain items

---
+++ AlexRecipeBookAPI/AlexRecipeBook.Domain/Recipe.cs
@@ -0,0 +1,28 @@
+﻿namespace AlexRecipeBook.Domain
+{
+    public class Recipe
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public long PreparationTime { get; set; }
+        public long CookingTime { get; set; }
+        public string SkillLevel { get; set; } = string.Empty;
+
+        private Recipe() { }
+
+        public static Recipe Create(string id, string name, string description, long preparationTime, long cookingTime,
+            string skillLevel)
+        {
+            return new Recipe
+            {
+                Id = id,
+                Name = name,
+                Description = description,
+                PreparationTime = preparationTime,
+                CookingTime = cookingTime,
+                SkillLevel = skillLevel
+            };
+        }
+    }
+}
