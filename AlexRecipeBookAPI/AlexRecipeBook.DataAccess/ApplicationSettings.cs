bacee21c4d21e214ce8ec0cce89759aa135a7665
alexa21e <pirvuletu.e.alexandra@gmail.com>
2024-11-16 14:13:15 +0200

    Add Neo4J Data Access Microservice

---
+++ AlexRecipeBookAPI/AlexRecipeBook.DataAccess/ApplicationSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace AlexRecipeBook.DataAccess
+{
+    public class ApplicationSettings
+    {
+        public Uri Neo4jConnection { get; set; }
+
+        public string Neo4jUser { get; set; }
+
+        public string Neo4jPassword { get; set; }
+
+        public string Neo4jDatabase { get; set; }
+    }
+}
