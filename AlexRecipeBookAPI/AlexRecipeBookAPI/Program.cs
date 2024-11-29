5828e2b24eab70a095350ba15bcc7ccd74054509
alexa21e <pirvuletu.e.alexandra@gmail.com>
2024-11-29 23:51:49 +0200

    Add CORS on backend

--- AlexRecipeBookAPI/AlexRecipeBookAPI/Program.cs
+++ AlexRecipeBookAPI/AlexRecipeBookAPI/Program.cs
@@ -14,6 +14,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CulinaryRecipes v1"));
 }
 
+app.UseCors("AllowFrontEnd");
+
 app.UseHttpsRedirection();
 
 app.UseRouting();
